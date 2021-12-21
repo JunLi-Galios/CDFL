7b8880cacc8e0f187d9334d1d2fa2e0b01ad2b26
Jun Li <29033600+JunLi-Galios@users.noreply.github.com>
2021-12-20 22:35:38 -0800

    Update network.py

--- utils/network.py
+++ utils/network.py
@@ -11,6 +11,66 @@ from .grammar import SingleTranscriptGrammar
 from .length_model import PoissonModel
 
 
+class MultiStageModel(nn.Module):
+    def __init__(self, num_stages, num_layers, num_f_maps, dim, num_classes):
+        super(MultiStageModel, self).__init__()
+        self.tower_stage = TowerModel(num_layers, num_f_maps, dim, num_classes)
+        self.single_stages = nn.ModuleList([copy.deepcopy(SingleStageModel(num_layers, num_f_maps, num_classes, num_classes, 3))
+                                     for s in range(num_stages-1)])
+
+    def forward(self, x, mask):
+        middle_out, out = self.tower_stage(x, mask)
+        outputs = out.unsqueeze(0)
+        for s in self.single_stages:
+            middle_out, out = s(F.softmax(out, dim=1) * mask[:, 0:1, :], mask)
+            outputs = torch.cat((outputs, out.unsqueeze(0)), dim=0)
+        return middle_out, outputs
+
+
+class TowerModel(nn.Module):
+    def __init__(self, num_layers, num_f_maps, dim, num_classes):
+        super(TowerModel, self).__init__()
+        self.stage1 = SingleStageModel(num_layers, num_f_maps, dim, num_classes, 3)
+        self.stage2 = SingleStageModel(num_layers, num_f_maps, dim, num_classes, 5)
+
+    def forward(self, x, mask):
+        out1, final_out1 = self.stage1(x, mask)
+        out2, final_out2 = self.stage2(x, mask)
+
+        return out1 + out2, final_out1 + final_out2
+
+
+class SingleStageModel(nn.Module):
+    def __init__(self, num_layers, num_f_maps, dim, num_classes, kernel_size):
+        super(SingleStageModel, self).__init__()
+        self.conv_1x1 = nn.Conv1d(dim, num_f_maps, 1)
+        self.layers = nn.ModuleList([copy.deepcopy(DilatedResidualLayer(2 ** i, num_f_maps, num_f_maps, kernel_size))
+                                     for i in range(num_layers)])
+        self.conv_out = nn.Conv1d(num_f_maps, num_classes, 1)
+
+    def forward(self, x, mask):
+        out = self.conv_1x1(x)
+        for layer in self.layers:
+            out = layer(out, mask)
+        final_out = self.conv_out(out) * mask[:, 0:1, :]
+        return out, final_out
+
+
+class DilatedResidualLayer(nn.Module):
+    def __init__(self, dilation, in_channels, out_channels, kernel_size):
+        super(DilatedResidualLayer, self).__init__()
+        padding = int(dilation + dilation * (kernel_size - 3) / 2)
+        self.conv_dilated = nn.Conv1d(in_channels, out_channels, kernel_size, padding=padding, dilation=dilation)
+        self.conv_1x1 = nn.Conv1d(out_channels, out_channels, 1)
+        self.dropout = nn.Dropout()
+
+    def forward(self, x, mask):
+        out = F.relu(self.conv_dilated(x))
+        out = self.conv_1x1(out)
+        out = self.dropout(out)
+        return (x + out) * mask[:, 0:1, :]
+
+
 # buffer for old sequences (robustness enhancement: old frames are sampled from the buffer during training)
 class Buffer(object):
 
